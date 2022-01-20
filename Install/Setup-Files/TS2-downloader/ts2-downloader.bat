d33e1be8cd6427016e9f4e68d622af2d436c043e
Jordan <GoTeamScotch@users.noreply.github.com>
2022-01-19 20:07:08 -0700

    Fix space issue
    Batch script can now be run from a path with spaces in it.

--- Install/Setup-Files/TS2-downloader/ts2-downloader.bat
+++ Install/Setup-Files/TS2-downloader/ts2-downloader.bat
@@ -7,10 +7,10 @@ set "steamuser=%~2"
 if "%~3"=="" echo Missing Steam password && exit /b 1
 set "steampass=%~3"
 
-%~dp0DepotDownloader.exe -app 223100 -depot 223101 -manifest 103344760119053156 -username %steamuser% -password %steampass% -dir "%installpath%"
-%~dp0DepotDownloader.exe -app 223100 -depot 223102 -manifest 3243358074883441516 -username %steamuser% -password %steampass% -dir "%installpath%"
-%~dp0DepotDownloader.exe -app 223100 -depot 223103 -manifest 8582653993015227315 -username %steamuser% -password %steampass% -dir "%installpath%" -filelist %~dp0223103-content.txt
-%~dp0DepotDownloader.exe -app 223100 -depot 223106 -manifest 1385625310001200108 -username %steamuser% -password %steampass% -dir "%installpath%"
+"%~dp0DepotDownloader.exe" -app 223100 -depot 223101 -manifest 103344760119053156 -username %steamuser% -password %steampass% -dir "%installpath%"
+"%~dp0DepotDownloader.exe" -app 223100 -depot 223102 -manifest 3243358074883441516 -username %steamuser% -password %steampass% -dir "%installpath%"
+"%~dp0DepotDownloader.exe" -app 223100 -depot 223103 -manifest 8582653993015227315 -username %steamuser% -password %steampass% -dir "%installpath%" -filelist %~dp0223103-content.txt
+"%~dp0DepotDownloader.exe" -app 223100 -depot 223106 -manifest 1385625310001200108 -username %steamuser% -password %steampass% -dir "%installpath%"
 
 echo Done!
 pause
