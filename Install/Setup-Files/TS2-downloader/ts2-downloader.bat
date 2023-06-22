a8a2827fa29ac9d9a016fd005636b1da087a2859
Jordan Jenson <GoTeamScotch@users.noreply.github.com>
2023-06-21 20:23:32 -0700

    TS2 Downloader update
    - Reduced number of calls to DepotDownloader. Users should only have to provide their password/Steam Guard code twice. DepotDownloader apparently supports remembering passwords, but I can't seem to get it to work. See pull 341 (SteamRE/DepotDownloader). For now, we'll just live with asking for the password/code twice, which is still less than before.
    - Added extra languages for non-english speaking users. Script will now download all languages, which only adds a few dozen more MB to the download size.

--- Install/Setup-Files/TS2-downloader/ts2-downloader.bat
+++ Install/Setup-Files/TS2-downloader/ts2-downloader.bat
@@ -4,13 +4,9 @@ if "%~1"=="" echo Missing install path && exit /b 1
 set "installpath=%~1"
 if "%~2"=="" echo Missing Steam username && exit /b 1
 set "steamuser=%~2"
-if "%~3"=="" echo Missing Steam password && exit /b 1
-set "steampass=%~3"
 
-"%~dp0DepotDownloader.exe" -app 223100 -depot 223101 -manifest 103344760119053156 -username %steamuser% -password %steampass% -dir "%installpath%"
-"%~dp0DepotDownloader.exe" -app 223100 -depot 223102 -manifest 3243358074883441516 -username %steamuser% -password %steampass% -dir "%installpath%"
-"%~dp0DepotDownloader.exe" -app 223100 -depot 223103 -manifest 8582653993015227315 -username %steamuser% -password %steampass% -dir "%installpath%" -filelist %~dp0223103-content.txt
-"%~dp0DepotDownloader.exe" -app 223100 -depot 223106 -manifest 1385625310001200108 -username %steamuser% -password %steampass% -dir "%installpath%"
+"%~dp0DepotDownloader.exe" -app 223100 -depot 223101 223102 223106 -username %steamuser% -remember-password -dir "%installpath%"
+"%~dp0DepotDownloader.exe" -app 223100 -depot 223103 -username %steamuser% -dir "%installpath%" -filelist %~dp0223103-content.txt
 
 echo Done!
 pause
